60f36c9d772bf2879c00320bac2b624346bc9aad
ORUÇHAN KURAL <17030411047@ybu.edu.tr>
2021-04-30 05:34:48 +0300

    Add project files.

---
+++ OHK/Controllers/SSSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OHK.Controllers
+{
+    public class SSSController : Controller
+    {
+        // GET: SSS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OHK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OHK.MvcApplication" Language="C#" %>
